f65e17185a4402aff700cb3bb8c8dd8e2c5a5c87
python533 <143389231+python533@users.noreply.github.com>
2024-03-14 16:57:35 +0300

    Class c++

---
+++ class.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class veriler(){
+public:
+  char isim[],adres[];
+}
+
+int(){
+bool kosul=true;
+
+while (!kosul) {
+std::cout << "İsim" << '\n';
+cin<<this.isim;
+std::cout << "Adres" << '\n';
+cin<<this.adres;
+cin.getline();
+
+
+return 0;
+}
+
+}
